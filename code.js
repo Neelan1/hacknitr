07e7d4b9914a0f2bbe1bbb34676c57a546c68689
Daniel-Tann <340883792@gapps.yrdsb.ca>
2023-01-06 17:00:40 -0500

    ;(

--- code.js
+++ code.js
@@ -1,90 +1,59 @@
-//For next couple days
-let location = "";
-let time = 0;
-// An array for all the events upcoming in the area
-let events = {
 
-}
-// An array for the time of when the events are going to occour
-let timeOfEvents = {
-
-}
-function getInfo()
-{
-  // location = document.getElementById('').value;//put the id of the input for location
-  // time = document.getElementById('').value; //put the id of the input for time
-  //Input info into the api
-  //Jave the api spit the info into the arrays
-  location = "Canada"
-  time = "20"
-}
-function getLocation(location) // gets location codes
-{
-  //returns location code
-}
-
-
-//returns an object which holds the results
-async function fetchQuote() // async means it happens with
-{
-    // gets the data from the website api using https://api.predicthq.com/v1/events/?category=disasters,terror,severe-weather, s
-    // it searches terror disasters, and severe weather everywhere all time 
-    
-    const response = await fetch('https://api.predicthq.com/v1/events/?category=disasters,terror,severe-weather' + place + date, options);
-    if (response.ok)  { // checks if response works
-        console.log("SUCCESS");
-        return await response.json(); // returns it
-    }
-    else {
-        console.log("FAIL"); // says if it fails
-
-    }
-}
-
-
-fetchQuote().then( // runs the api fetch and gets the info
-
-    // after its done loading it does all the stuff in the brackets
-    response => {   
-        console.log(response);
-        events = response.results
-        console.log(events);
-    }
-
-
-);
-
+let container = document.getElementsByClassName("container-1");
 function putSnowStorm(time){
     const img = document.createElement("img");
+    img.setAttribute('id',"thunderstorm" + val);
     img.src = "snow storm.jpg";
-    document.body.appendChild(img);
+    container = document.getElementsByClassName("container-1");
+    document.container.appendChild(img);
     document.write("<br /> SnowStorm On " + time);
+    document.appendChild()
 }
 
-function putThunderStorm(){
+function putThunderStorm(time){
     const img = document.createElement("img");
+    img.setAttribute('id',"thunderstorm" + val);
     img.src = "Thunderstorm.webp";
-    document.body.appendChild(img);
+    container = document.getElementsByClassName("container-1");
+    document.container.appendChild(img);
     document.write("<br /> Thunderstorm On " + time);
+    val += 1;
 }
-function putFlood(){
+function putFlood(time){
     const img = document.createElement("img");
     img.src = "Flood.webp";
-    document.body.appendChild(img);
+    container = document.getElementsByClassName("container-1");
+    document.container.appendChild(img);
     document.write("<br /> Flood On " + time);
 }
-function putHeatWave(){
+function putHeatWave(time){
     const img = document.createElement("img");
     img.src = "Heatwave.jpg";
-    document.body.appendChild(img);
+    container = document.getElementsByClassName("container-1");
+    document.container.appendChild(img);
     document.write("<br /> Heatwave On " + time);
 }
-function putTornado(){
-    const img = document.createElement("img");
-    img.src = "Tornado.jpg";
-    document.body.appendChild(img);
-    document.write("<br /> Tornado On " + time);
-}
+function putTornado(time){
+    // let img = document.createElement("IMG");
+    // img.src = "Tornado.jpg";
+    // document.body.appendChild(img);
+    // document.write("<br /> Tornado On " + time);
+  
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "Tornado.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+   
+    const para = document.createElement("p");
+    para.innerHTML = "Tornado On " + time;
+    document.body.append(para);
+  }
+
+  
+
+
+
 
 
 
