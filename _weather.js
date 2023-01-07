268d43c205d1ea55efe296d81c586cd0bd0b033b
Neelan <neelan2317@gmail.com>
2023-01-07 17:43:08 -0500

    '_weather.js'

--- _weather.js
+++ _weather.js
@@ -81,144 +81,144 @@ function getCalender(){
         
     }
 }
-let counter = 0;
+let counter1 = 0, counter2 = 0, counter3 = 0, counter4 = 0, counter5 = 0, counter6 = 0, counter7 = 0, counter8 = 0;
 function displayText(id)
 {
-    if( counter == 0)
+   
+   
+    const weatherArray1 = ['Temperature (C): ', 'Wind (kph): ', 'Conditions: '];
+
+    if(id.includes('2') && counter2 == 0)
     {
-        counter++;
-        const weatherArray1 = ['Temperature (C): ', 'Wind (kph): ', 'Conditions: '];
+        counter2++;
+
+        const weatherArray2 = [weatherList[1].avgtemp_c, weatherList[1].maxwind_kph, weatherList[1].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            console.log(text)
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
+
+        }
+
+    }
+    if(id.includes('1') && counter1 == 0)
+    {
+        counter1++;
+
         
-        if(id.includes('1'))
-        {
-
-            
-            const weatherArray2 = [weatherList[0].temp_c, weatherList[0].wind_kph, weatherList[0].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
-
-            }
+        const weatherArray2 = [weatherList[0].temp_c, weatherList[0].wind_kph, weatherList[0].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
 
         }
-        if(id.includes('2'))
-        {
 
-            
-            const weatherArray2 = [weatherList[1].temp_c, weatherList[1].wind_kph, weatherList[1].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
+    }
+    if(id.includes('3') && counter3 == 0)
+    {
 
-            }
-
-        }
-        if(id.includes('3'))
-        {
-
-            
-            const weatherArray2 = [weatherList[2].temp_c, weatherList[2].wind_kph, weatherList[2].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
-
-            }
+        counter3++;
+        const weatherArray2 = [weatherList[2].avgtemp_c, weatherList[2].maxwind_kph, weatherList[2].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
 
         }
-        if(id.includes('4'))
-        {
 
-            
-            const weatherArray2 = [weatherList[3].temp_c, weatherList[3].wind_kph, weatherList[3].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
+    }
+    if(id.includes('4') && counter4 == 0)
+    {
+        counter4++;
 
-            }
-
-        }
-        if(id.includes('5'))
-        {
-
-            
-            const weatherArray2 = [weatherList[4].temp_c, weatherList[4].wind_kph, weatherList[4].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
-
-            }
+        
+        const weatherArray2 = [weatherList[3].avgtemp_c, weatherList[3].maxwind_kph, weatherList[3].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
 
         }
-        if(id.includes('6'))
-        {
 
-            
-            const weatherArray2 = [weatherList[5].temp_c, weatherList[5].wind_kph, weatherList[5].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
+    }
+    if(id.includes('5') && counter5 == 0)
+    {
+        counter5++;
 
-            }
-
-        }
-        if(id.includes('7'))
-        {
-
-            
-            const weatherArray2 = [weatherList[6].temp_c, weatherList[6].wind_kph, weatherList[6].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
-
-            }
+        
+        const weatherArray2 = [weatherList[4].avgtemp_c, weatherList[4].maxwind_kph, weatherList[4].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
 
         }
-        if(id.includes('8'))
-        {
 
-            
-            const weatherArray2 = [weatherList[7].temp_c, weatherList[7].wind_kph, weatherList[7].condition.text]
-            for(let i = 0; i <weatherArray2.length; i++ ){
-                let text = document.createElement("li");
-                text.innerHTML = weatherArray1[i] + weatherArray2[i];
-                document.getElementById(id).appendChild(text);
+    }
+    if(id.includes('6') && counter6 == 0) 
+    {
+        counter6++;
 
-            }
+        const weatherArray2 = [weatherList[5].avgtemp_c, weatherList[5].maxwind_kph, weatherList[5].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
+
+        }
+
+    }
+    if(id.includes('7') && counter7 == 0)
+    {
+
+        counter7++;
+        const weatherArray2 = [weatherList[6].avgtemp_c, weatherList[6].maxwind_kph, weatherList[6].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
+
+        }
+
+    }
+    if(id.includes('8') && counter8 == 0)
+    {
+
+        counter8++;
+        const weatherArray2 = [weatherList[7].avgtemp_c, weatherList[7].maxwind_kph, weatherList[7].condition.text]
+        for(let i = 0; i <weatherArray2.length; i++ ){
+            let text = document.createElement("li");
+            text.innerHTML = weatherArray1[i] + weatherArray2[i];
+            document.getElementById(id).appendChild(text);
 
         }
 
     }
 
-
-
 }
-function stopText()
-{
-    
 
 
-    
 
 
-}
+
 let weatherList;
 fetchWeather(JSON.parse(localStorage.getItem('longlat'))).then( // runs the api fetch and gets the info
 
         // after its done loading it does all the stuff in the brackets
         (response) => {
             let events =[response.current]
-            let events2 = response.forecast.forecastday
+            
+            let events2 = [];
+            for(let i = 0; i <response.forecast.forecastday.length; i++ ){
+                events2.push(response.forecast.forecastday[i].day)
+
+            }
             Array.prototype.push.apply(events, events2)
            
             getCalender();
-            console.log(events)
+            
             weatherList = events;
 
 
