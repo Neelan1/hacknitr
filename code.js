5b07bf28f60baaa6763b375bfe3f26373880fcc2
Neelan <neelan2317@gmail.com>
2023-01-06 20:39:45 -0500

    Add working button

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 
 let container = document.getElementsByClassName("container-1");
-const form = document.querySelector('.form__group');
-console.log(form);
+
+
 const currentDate = new Date();
 const predictedDate = new Date();
-predictedDate.setDate(predictedDate.getDate() + 75);
+predictedDate.setDate(predictedDate.getDate() + 90);
 
 const options = { // to use api
 
@@ -14,25 +14,21 @@ const options = { // to use api
 }
 function getInfo()
 {
-  // location = document.getElementById('').value;//put the id of the input for location
-  // time = document.getElementById('').value; //put the id of the input for time
-  //Input info into the api
-  //Jave the api spit the info into the arrays
-  location = "Canada"
-  time = "20"
-}
-function getLocation(location) // gets location codes
-{
 
 
-  
+    
+    //Input info into the api
+    //Jave the api spit the info into the arrays
+
+
 }
-async function fetchLongLat() // async means it happens with
+
+async function fetchLongLat(country, city) // async means it happens with
 {
     // gets the data from the website api using https://api.predicthq.com/v1/events/?category=disasters,terror,severe-weather, s
     // it searches terror disasters, and severe weather everywhere all time 
     
-    const response = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Moscow,RU&limit=1&appid=4466ecaa9785525d41f5b0a40540b3c0');
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=4466ecaa9785525d41f5b0a40540b3c0`);
     if (response.ok)  { // checks if response works
         
         return await response.json(); // returns it
@@ -55,7 +51,7 @@ async function fetchQuote(place) // async means it happens with
         + '&category=disasters,terror,severe-weather' 
         + '&start.lte='
         + `${predictedDate.getFullYear()}-${predictedDate.getMonth() + 1}-${predictedDate.getDate()}`
-        + '&start.gte='
+        + '&start.gt='
         + `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`
         + '&within=300km@'+ place[0] + ',' + place[1] 
         , options);
@@ -68,77 +64,118 @@ async function fetchQuote(place) // async means it happens with
 
     }
 }
-//For next couple days
-let place = "";
-let time = 0;
-// An array for all the events upcoming in the area
-let events = {
-
-}
-// An array for the time of when the events are going to occour
-let timeOfEvents = {
-
-}
-fetchLongLat().then( // runs the api fetch and gets the info
-
-    // after its done loading it does all the stuff in the brackets
-    response => {  
-
-        const longlat = [response[0].lat, response[0].lon];
-        console.log(longlat);
-        fetchQuote(longlat).then( // runs the api fetch and gets the info
-
-            // after its done loading it does all the stuff in the brackets
-            response => {   
-                events = response.results
-                localStorage.setItem('events', JSON.stringify(events));
 
 
-            }
-        );
-    }
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) =>
+{
+    e.preventDefault();
+    const country = document.getElementById('country').value; //put the id of the input for location
+    const city = document.getElementById('city').value; 
+    fetchLongLat(country, city).then( // runs the api fetch and gets the info
+
+        // after its done loading it does all the stuff in the brackets
+        response => {  
+
+            const longlat = [response[0].lat, response[0].lon];
+            console.log(longlat);
+            fetchQuote(longlat).then( // runs the api fetch and gets the info
+
+                // after its done loading it does all the stuff in the brackets
+                response => {   
+                    events = response.results
+                    console.log(events);
+                    localStorage.setItem('events', JSON.stringify(events));
 
 
-);
+                }
+            );
+        }
 
+
+    )
+})
 
 
 
 
-function putSnowStorm(time){
-    const img = document.createElement("img");
-    img.setAttribute('id',"thunderstorm" + val);
-    img.src = "snow storm.jpg";
-    container = document.getElementsByClassName("container-1");
-    document.container.appendChild(img);
-    document.write("<br /> SnowStorm On " + time);
-    document.appendChild()
+
+
+function putSnowStorm(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "snow storm.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
 }
 
-function putThunderStorm(time){
-    const img = document.createElement("img");
-    img.setAttribute('id',"thunderstorm" + val);
-    img.src = "Thunderstorm.webp";
-    container = document.getElementsByClassName("container-1");
-    document.container.appendChild(img);
-    document.write("<br /> Thunderstorm On " + time);
-    val += 1;
+function putThunderStorm(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "Thunderstorm.webp");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+   
 }
-function putFlood(time){
-    const img = document.createElement("img");
-    img.src = "Flood.webp";
-    container = document.getElementsByClassName("container-1");
-    document.container.appendChild(img);
-    document.write("<br /> Flood On " + time);
+function putFlood(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "Flood.webp");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+   
 }
-function putHeatWave(time){
-    const img = document.createElement("img");
-    img.src = "Heatwave.jpg";
-    container = document.getElementsByClassName("container-1");
-    document.container.appendChild(img);
-    document.write("<br /> Heatwave On " + time);
+function putHeatWave(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "Heatwave.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+    
 }
-function putTornado(time){
+function putEarthquake(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "earthquake.webp");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function putRain(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "rain.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function putDust(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "dust.webp");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function putEruption(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "erruption.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function putHurricane(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "hurricane.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function noImage(){
+    let x = document.createElement("IMG");
+    x.setAttribute("src", "none.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+}
+function putTornado(){
     // let img = document.createElement("IMG");
     // img.src = "Tornado.jpg";
     // document.body.appendChild(img);
@@ -149,24 +186,43 @@ function putTornado(time){
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
     document.body.appendChild(x);
-   
-    const para = document.createElement("p");
-    para.innerHTML = "Tornado On " + time;
-    document.body.append(para);
   }
-  
+
   
 
   function printResults(){
     for(let i = 0; i < events.length; i++){
-        
+       if(events[i].title.toLowerCase.includes("earthquake"))
+            putEarthquake()
+       if(events[i].title.includes("Snowstorm") || events[i].title.includes("Winter Storm") || events[i].title.includes("Ice"))
+            putSnowStorm();
+       if(events[i].title.toLowerCase.includes("thunderstorm"))
+            putThunderstorm();
+       if(events[i].title.toLowerCase.includes("flood"))
+            putFlood();
+       if(events[i].title.toLowerCase.includes("heatWave"))
+            putHeatWave();
+       if(events[i].title.toLowerCase.includes("tornado") || events[i].title.includes("wind"))
+           putTornado();
+       if(events[i].title.toLowerCase.includes("rain"))
+           putRain();
+       if(events[i].title.toLowerCase.includes("dust") || events[i].title.toLowerCase.includes("sandstorm"))
+            putDust();
+       if(events[i].title.toLowerCase.includes("eruption"))
+           putEruption();
+       if(events[i].title.toLowerCase.includes("hurricane") || events[i].title.toLowerCase.includes("tropical storm") || events[i].title.toLowerCase.includes("typhoon") || events[i].title.toLowerCase.includes("cyclone"))
+           putHurricane();
+        else{
+            noImage();
+            const para = document.createElement("p");
+            para.innerHTML = events[i].title +" on " + events[i].start;
+            document.body.append(para);
+        }
+        const para = document.createElement("p");
+        para.innerHTML = events[i].title +" on " + events[i].start;
+        document.body.append(para);
     }
   }
-
-  
-
-
-
 
 
 
